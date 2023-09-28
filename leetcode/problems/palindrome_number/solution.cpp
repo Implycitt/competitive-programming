7322e418b35b06167625d474aa4a9257d57ea927
Implycitt <qgbordelon@gmail.com>
2023-09-28 02:48:07 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

---
+++ leetcode/problems/palindrome_number/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        if (x != 0 && x % 10 == 0) {
+            return false;
+        }
+
+        int reversed = 0;
+
+        while (x > reversed) {
+            reversed = reversed*10 + x % 10;
+            x /= 10;
+        }
+ 
+        if (reversed == x || x == reversed/10) {
+            return true;
+        }
+
+        return false;
+    }
+};
