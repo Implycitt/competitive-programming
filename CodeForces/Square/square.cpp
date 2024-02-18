9cda1f38aa032c42eca56034fb2aefe79d121b18
Implycitt <qgbordelon@gmail.com>
2024-02-17 23:13:54 -0600

    Square

---
+++ CodeForces/Square/square.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    //do tomorrow apparently I can't find the area of a damn square right now.
+  }
+}
