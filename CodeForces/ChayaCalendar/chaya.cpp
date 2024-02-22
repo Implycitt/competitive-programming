3c5158eb75548f198115c9fa68f8b0bfe484182f
Implycitt <qgbordelon@gmail.com>
2024-02-21 21:45:57 -0600

    Chaya Calendar

--- CodeForces/ChayaCalendar/chaya.cpp
+++ CodeForces/ChayaCalendar/chaya.cpp
@@ -5,6 +5,14 @@ int main() {
   int t;
   cin >> t;
   while (t--) {
-    
+    int n;
+    cin >> n;
+    int cur = 0;
+    for (int i = 0; i < n; ++i) {
+      int x;
+      cin >> x;
+      cur += x - cur % x;
+    } 
+    cout << cur << '\n';
   }
 }
