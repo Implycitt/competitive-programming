3362e36f114eefeb6a8cfdc3f35e2e035732e61f
Implycitt <qgbordelon@gmail.com>
2024-03-08 21:02:43 -0600

    Turtle Fingers

--- CodeForces/TurtleFingers/turtle.cpp
+++ CodeForces/TurtleFingers/turtle.cpp
@@ -2,21 +2,34 @@
 
 using namespace std;
 
-int main() {
-  int t;
-  cin >> t;
-  while (t--) {
-    int a, b, l, sum = 0;
+void solve(){
+    int a, b, l;
     cin >> a >> b >> l;
-    for (int i = 0; i < 100; ++i) {
-      for (int x = 0; x <= 100; ++x) {
-        for (int y = 0; y <= 100; ++y) {
-          if ((int)(i * ((int)pow(a, x)) * ((int) pow(b, y))) == l) {
-            sum++;
-          }
+    set<int> ans;
+    for(int i = 0; i <= 34; ++i){
+        int x = l;
+        bool fail = false;
+        for(int j = 0; j < i; ++j){
+            if(x % a){
+                fail = true;
+                break;
+            }
+            x /= a;
         }
-      }
+        if(fail) break;
+        while(true){
+            ans.insert(x);
+            if(x % b) break;
+            x /= b;
+        }
     }
-    cout << sum << '\n';
-  }
+    cout << ans.size() << '\n';
+}
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
 }
