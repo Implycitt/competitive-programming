c7e15feb61131e3c3f68871da11f8dec1a14eea3
Implycitt <qgbordelon@gmail.com>
2024-05-04 09:44:41 -0500

    Sum Square Difference

---
+++ ProjectEuler/SumSquareDifference/sum.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll unsigned long long
+
+int main() {
+  ll sumSquares, squareSums, num = 100;
+  sumSquares = num * (num + 1) * (2 * num + 1) / 6;
+  squareSums = num * (num + 1) / 2;
+  squareSums = pow(squareSums, 2);
+  cout << squareSums-sumSquares;
+}
