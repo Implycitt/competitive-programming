dc32ed6d48c582d376c83a98b9889772ea845e09
Implycitt <qgbordelon@gmail.com>
2024-11-25 23:48:07 -0600

    2020 day 4 part 2

--- AOC/2020/4.py
+++ AOC/2020/4.py
@@ -1,6 +1,7 @@
+import re
+
 with open("../in.txt", 'r') as file:
     text = file.read().split('\n\n')
-    text = [i.replace('\n', ' ') for i in text]
 
 def part1():
     sum = 0
@@ -15,20 +16,18 @@ def part1():
     print(sum)
 
 def part2():
-    sum = 0
-    fields = {'byr': (1920, 2002), 'iyr': (2010, 2020), 'eyr': (2020, 2030), 'hgt': int, 'hcl': str, 'ecl': ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'), 'pid': int}
-    for entry in text:
-        pp = {}
-        entries = entry.split(' ')
-        for i in entries:
-            key, value = i.split(':')
-            pp[key] = value
-        valid = True
+    fields = {
+            "byr": r"byr:\s*(19[2-9]\d|200[0-2])\b", 
+            "iyr": r"iyr:\s*20(1\d|20)\b",
+            "eyr": r"eyr:\s*20(2\d|30)\b",
+            "hgt": r"hgt:\s*(1([5-8]\d|9[0-3])cm|(59|6\d|7[0-6])in)",
+            "hcl": r"hcl:\s*#[0-9a-f]{6}\b",
+            "ecl": r"ecl:\s*(amb|blu|brn|gry|grn|hzl|oth)\b",
+            "pid": r"pid:\s*\d{9}\b"
+    }
 
-
-
-        if valid:
-            sum += 1
-
+    print(sum([all([re.search(fields[k], p) for k in fields]) for p in text]))
+            
 if __name__ == "__main__":
+   part1()
    part2() 
