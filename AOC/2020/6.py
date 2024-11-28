ebc17e8061b678335811b580b648a5fbcbbbb664
Implycitt <qgbordelon@gmail.com>
2024-11-27 20:53:27 -0600

    day 6 2020

--- AOC/2020/6.py
+++ AOC/2020/6.py
@@ -1,8 +1,35 @@
 with open("../inputs/2020/6.txt") as file:
-    text = file.read().splitlines()
+    text = file.read().split('\n\n')
+    text = [i.split('\n') for i in text]
+    text = [list(filter(None, i)) for i in text]
 
 def part1():
-    return
+    s = []
+    for group in text:
+        answers = set()
+        for person in group:
+            for answer in person:
+                answers.add(answer)
+        s.append(len(answers))
+    print(sum(s))
 
+def part2():
+    s = 0 
+    for group in text:
+        ans = []
+        answers = set()
+        i = 0
+        for person in group:
+            i += 1
+            for answer in person:
+                ans.append(answer)
+                answers.add(answer)
+        for k in answers:
+            if ans.count(k) == i:
+                s += 1
+
+    print(s)
+    
 if __name__ == "__main__":
     part1()
+    part2()
