826a4b565108a7777589749b9d71a171bf629ee7
Implycitt <qgbordelon@gmail.com>
2024-11-30 15:17:18 -0600

    some more problems or something

--- everybodyCodes/KingdomOfAlgorithmia/3.py
+++ everybodyCodes/KingdomOfAlgorithmia/3.py
@@ -1,34 +1,69 @@
-map = []
-with open("test.txt", 'r') as file:
-    map = [[ch] for ch in [line.strip() for line in file]]
-
 def part1():
-    flag = True
-    while (flag):
-        changed = 0
-        newmap = []
-        for y, lines in enumerate(map):
-            for line in lines:
-                newline = list(line)
-                for x, ch in enumerate(line):
-                    top = map[y+1][0][x] #this is going to out of bounds uggh
-                    bottom = map[y-1][0][x]
-                    left = map[y][0][x-1]
-                    right = map[y][0][x+1]
-                    if (ch == '#'):
-                        newline[x] = 1
-                        changed += 1
-                    if (ch == '.'):
-                        newline[x] = 0
-                    # if (true):
-                    #     continue
-                    #     newline[x] -= 1
-                    #     changed += 1
-                    if changed == 0:
-                        flag = False
-                newmap.append(newline)
-        currmap = newmap
-        print(currmap)
+    with open('input.txt', 'r') as file:
+        grid = [list(i) for i in file.read().splitlines()]
+
+    row = len(grid)
+    col = len(grid[0])
+    neigh = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    grid = [[0 if grid[r][c] == '.' else 1 for c in range(col)] for r in range(row)]
+    next = [[0 for _ in r] for r in grid]
+    s = sum([sum([grid[r][c] for c in range(col)]) for r in range(row)])
+
+
+    height = 2 
+    while True:
+        changed = False
+        for r in range(row):
+            for c in range(col):
+                adj = [grid[r+y][c+x] for x, y in neigh if 0 <= r + y < row and 0 <= c + x < col]
+                diff = 0
+                for i in adj:
+                    diff = max(diff, abs(int(grid[r][c] - int(i))))
+                if diff < 1 and grid[r][c] != 0:
+                    s += 1
+                    changed = True
+                    next[r][c] = height
+        if not changed:
+            break
+        
+        height += 1
+        grid = [[next[r][c] for c in range(col)] for r in range(row)]
+
+    print(s)
+
+def part3():
+    with open('input.txt', 'r') as file:
+        grid = [list(i) for i in file.read().splitlines()]
+
+    row = len(grid)
+    col = len(grid[0])
+    neigh = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    grid = [[0 if grid[r][c] == '.' else 1 for c in range(col)] for r in range(row)]
+    next = [[0 for _ in r] for r in grid]
+    s = sum([sum([grid[r][c] for c in range(col)]) for r in range(row)])
+
+
+    height = 2 
+    while True:
+        changed = False
+        for r in range(row):
+            for c in range(col):
+                adj = [grid[r+y][c+x] for x, y in neigh if 0 <= r + y < row and 0 <= c + x < col]
+                diff = 0
+                for i in adj:
+                    diff = max(diff, abs(int(grid[r][c] - int(i))))
+                if diff < 1 and grid[r][c] != 0:
+                    s += 1
+                    changed = True
+                    next[r][c] = height
+        if not changed:
+            break
+        
+        height += 1
+        grid = [[next[r][c] for c in range(col)] for r in range(row)]
+
+    print(s)
 
 if __name__ == "__main__":
     part1()
+    part3()
