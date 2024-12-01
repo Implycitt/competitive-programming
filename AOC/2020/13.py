3cba7f76e68e01748fedbca8653d0eebdfff705f
Implycitt <qgbordelon@gmail.com>
2024-12-01 15:42:30 -0600

    small commit

---
+++ AOC/2020/13.py
@@ -0,0 +1,12 @@
+with open('../inputs/2020/13.txt', 'r') as file:
+    text = file.read().splitlines()
+
+def part1():
+    print(text)
+
+def part2():
+    return
+
+if __name__ =="__main__":
+    part1()
+    part2()
