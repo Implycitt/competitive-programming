1ab8b9ad4a4a1b024fbf8dc14a4508c6bdee86fa
Implycitt <qgbordelon@gmail.com>
2024-12-02 23:33:51 -0600

    2024 day 3

---
+++ AOC/2024/3.py
@@ -0,0 +1,31 @@
+import re
+with open("../inputs/2024/3.txt", 'r') as file:
+    data = file.read().rstrip()
+
+def part1():
+    s = 0
+    muls = re.findall("(mul)\((\d+),(\d+)\)", data)
+    for mul, a, b in muls:
+        if mul:
+            s += int(a) * int(b)
+
+    print(s)
+
+def part2():
+    s = 0
+    y = True
+    muls = re.findall("(mul)\((\d+),(\d+)\)|(do)\(\)|(don)'t\(\)", data)
+    for mul, a, b, do, dont in muls:
+        if mul:
+            if y:
+                s += int(a) * int(b)
+        elif do:
+            y = True
+        elif dont:
+            y = False
+
+    print(s)
+
+if __name__ == "__main__":
+    part1()
+    part2()
