3ebb564a3781cfe2fcf8db346812ad0e4d4a0b78
Implycitt <qgbordelon@gmail.com>
2024-12-04 12:21:03 -0600

    2024 day 4

--- AOC/2024/4.py
+++ AOC/2024/4.py
@@ -1,48 +1,45 @@
-with open("../inputs/2024/test.txt", 'r') as file:
+with open("../inputs/2024/4.txt", 'r') as file:
     data = file.read().splitlines()
 
 def part1():
     ans = 0
     rows = len(data)
     cols = len(data[0])
+    w = []
+    xdirs = [-1, 0, 1]
+    ydirs = [-1, 0, 1]
 
     for x in range(rows):
         for y in range(cols):
-            w = ''
-            ws = []
-            if x+3 < rows and y+3 < cols:
-                w = data[x][y] + data[x+1][y+1] + data[x+2][y+2] + data[x+3][y+3] 
-                ws.append(w)
-            if x+3 < rows and y-3 >= 0:
-                w = data[x][y] + data[x-1][y-1] + data[x-2][y-2] + data[x+3][y-3] 
-                ws.append(w)
-            if x-3 >= 0 and y+3 < cols:
-                w = data[x][y] + data[x-1][y+1] + data[x-2][y+2] + data[x-3][y+3] 
-                ws.append(w)
-            if x-3 >= 0 and y-3 >= 0:
-                w = data[x][y] + data[x-1][y-1] + data[x-2][y-2] + data[x-3][y-3] 
-                ws.append(w)
-            if x < rows-3:
-                w = data[x][y] + data[x+1][y] + data[x+2][y] + data[x+3][y] 
-                ws.append(w)
-            if x >= 0:
-                w = data[x][y] + data[x-1][y] + data[x-2][y] + data[x-3][y] 
-                ws.append(w)
-            if y < cols-3:
-                w = data[x][y] + data[x][y-1] + data[x][y+2] + data[x][y+3] 
-                ws.append(w)
-            if y >= 0:
-                w = data[x][y] + data[x][y-1] + data[x][y-2] + data[x][y-3] 
-                ws.append(w)
+            for dx in xdirs:
+                for dy in ydirs:
+                    if dx == dy == 0: 
+                        continue
+                    if (0 <= x+3*dx < rows and 0 <= y+3*dy < cols):
+                        w.append(data[x][y] + data[x+dx][y+dy] + data[x+2*dx][y+2*dy] + data[x+3*dx][y+3*dy])
 
-            for i in ws:
-                if i == 'XMAS':
-                    ans += 1
+    for i in w:
+        if i == "XMAS":
+            ans += 1
 
     print(ans)
 
 def part2():
     ans = 0
+    rows = len(data)
+    cols = len(data[0])
+    grids = []
+    poss = ["MMASS", "MSAMS", "SSAMM", "SMASM"]
+
+    for x in range(rows):
+        for y in range(cols):
+            if (x+2 < rows and y+2 < cols):
+                grids.append([data[x][y], data[x][y+2]]+[data[x+1][y+1]]+[data[x+2][y], data[x+2][y+2]])
+    
+    for i in grids:
+        cross = ''.join(i) 
+        if cross in poss:
+            ans += 1
 
     print(ans)
 
