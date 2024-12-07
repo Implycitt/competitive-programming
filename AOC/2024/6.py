065ddf2179a46d9f71c64d033c47f55b2afcb811
Implycitt <qgbordelon@gmail.com>
2024-12-06 22:57:17 -0600

    2024 day 6

--- AOC/2024/6.py
+++ AOC/2024/6.py
@@ -1,4 +1,4 @@
-with open("../inputs/2024/test.txt", 'r') as file:
+with open("../inputs/2024/6.txt", 'r') as file:
     data = file.read().splitlines()
 
 def part1():
@@ -24,36 +24,41 @@ def part1():
     ans = sum([sum([1 if j == 'X' else 0 for j in i ]) for i in grid])
     print(ans)
 
+def check(grid, r, c):
+    row, col = len(data), len(data[0])
+    alr = set() 
+
+    dr, dc = -1, 0 
+    while True:
+        alr.add((r, c, dr, dc))
+        if r+dr < 0 or r+dr >= row or c+dc < 0 or c+dc >= col: return False 
+        if grid[r+dr][c+dc] == '#':
+            dc, dr = -dr, dc 
+        else:
+            c += dc 
+            r += dr 
+        if ((r, c, dr, dc)) in alr:
+            return True
+        
+
 def part2():
     ans = 0
-    gx, gy, dir = 0, 0, 0
-    dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
     row, col = len(data), len(data[0])
     grid = [[x for x in y] for y in data]
-    for i in range(row):
-        for j in range(col):
-            if grid[i][j] == '^':
-                gy, gx = i, j
-
-    for i in range(row):
-        for j in range(col):
-            if grid[i][j] == '#' or grid[i][j] == '^': continue
-            grid[i][j] = '#'
-            steps = 0
-            while (0 <= gx < row and 0 <= gy < col or steps < row*col):
-                steps += 1
-                dx, dy = dirs[dir]
-                if 0 <= gy+dy < row and 0 <= gx+dx < col and grid[gy+dy][gx+dx] == '#':
-                    dir = (dir+1)%4
-                    continue
-                gx += dx
-                gy += dy
-            if steps >= row*col:
+    r, c = 0, 0
+    for x in range(row):
+        for y in range(col):
+            if grid[x][y] == '^':
+                r, c = x, y 
+    
+    for dr in range(row):
+        for dc in range(col):
+            if grid[dr][dc] != ".": continue
+            grid[dr][dc] = "#"
+            if check(grid, r, c):
                 ans += 1
-            grid = [[x for x in y] for y in data]
+            grid[dr][dc] = "."
             
-
-
     print(ans)
 
 if __name__ == "__main__":
---
+++ AOC/2024/7.py
@@ -0,0 +1,16 @@
+with open("../inputs/2024/7.txt", 'r') as file:
+    data = file.read().splitlines()
+
+def part1():
+    ans = 0
+
+    print(ans)
+
+def part2():
+    ans = 0
+
+    print(ans)
+
+if __name__ == "__main__":
+    part1()
+    part2()
