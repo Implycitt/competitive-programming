7c48c29411f16202a467b085aba65aef3dcc5d9f
Implycitt <qgbordelon@gmail.com>
2024-12-07 22:28:13 -0600

    2024 day 7

--- AOC/2024/7.py
+++ AOC/2024/7.py
@@ -3,12 +3,35 @@ with open("../inputs/2024/7.txt", 'r') as file:
 
 def part1():
     ans = 0
-
+    p = 1
+    for i in data:
+        res, nums = i.split(': ')
+        res = int(res)
+        nums = [int(x) for x in nums.split(' ')]
+        if check(res, nums, p):
+            ans += res
+            
     print(ans)
+
+def check(ans, n, p):
+    if len(n) == 1: return n[0] == ans
+    if ans % n[-1] == 0 and check(ans // n[-1], n[:-1], p): return True
+    if ans > n[-1] and check(ans - n[-1], n[:-1], p): return True
+    if len(str(ans)) > len(str(n[-1])) and p == 2:
+        if str(ans).endswith(str(n[-1])):
+            if check(int(str(ans)[:-len(str(n[-1]))]), n[:-1], p): return True
+    return False
 
 def part2():
     ans = 0
-
+    p = 2
+    for i in data:
+        res, nums = i.split(': ')
+        res = int(res)
+        nums = [int(x) for x in nums.split(' ')]
+        if check(res, nums, p):
+            ans += res
+            
     print(ans)
 
 if __name__ == "__main__":
