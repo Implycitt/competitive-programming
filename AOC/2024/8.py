3b2868203bc5ce88e4d4457e2c040701f9890656
Implycitt <qgbordelon@gmail.com>
2024-12-08 22:02:23 -0600

    2024 day 8

--- AOC/2024/8.py
+++ AOC/2024/8.py
@@ -1,4 +1,4 @@
-with open("../inputs/2024/test.txt", 'r') as file:
+with open("../inputs/2024/8.txt", 'r') as file:
     data = file.read().splitlines()
 
 def part1():
@@ -6,14 +6,14 @@ def part1():
     col = len(data[0])
     nodes = dict() 
     antinodes = set()
+
     for r in range(row):
         for c in range(col):
             if data[r][c] == '.': continue
             node = data[r][c]
             if node not in nodes.keys(): 
-                nodes[node] = [(c, r)]
-            else:
-                nodes[node].append((c, r))
+                nodes[node] = []
+            nodes[node].append((c, r))
 
     for key in nodes:
         for x in range(len(nodes[key])):
@@ -37,38 +37,28 @@ def part2():
     col = len(data[0])
     nodes = dict() 
     antinodes = set()
+
     for r in range(row):
         for c in range(col):
             if data[r][c] == '.': continue
             node = data[r][c]
             if node not in nodes.keys(): 
-                nodes[node] = [(c, r)]
-            else:
-                nodes[node].append((c, r))
+                nodes[node] = []
+            nodes[node].append((c, r))
 
-    grid = [[x for x in i] for i in data]
-    for key in nodes:
-        for x in range(len(nodes[key])):
-            for y in range(x+1, len(nodes[key])):
-                x1, y1 = nodes[key][x] 
-                x2, y2 = nodes[key][y] 
-                dy, dx = y2-y1, x2-x1
-                antinode1 = (x1-dx, y1-dy)
-                antinode2 = (x2+dx, y2+dy)
-                
-                while 0 <= antinode1[0]-dx < col and 0 <= antinode1[1]-dy < row: 
-                    # print(antinode1[0]+dx, antinode1[1]+dy)
-                    grid[antinode1[1]][antinode1[0]] = '#'
-                    antinodes.add(antinode1)
-                    antinode1 = (antinode1[0]-dx, antinode1[1]-dy)
-                while 0 <= antinode2[0]+dx < col and 0 <= antinode2[1]+dy < row:
-                    # print(antinode2[0]+dx, antinode2[1]+dy)
-                    grid[antinode2[1]][antinode2[0]] = '#'
-                    antinodes.add(antinode2)
-                    antinode2 = (antinode2[0]-dx, antinode2[1]-dy)
-
-    # for i in grid:
-    #     print(i)
+    for array in nodes.values():
+        for i in range(len(array)):
+            for j in range(len(array)):
+                if i == j: continue
+                y1, x1 = array[i]
+                y2, x2 = array[j]
+                dy = y2 - y1
+                dx = x2 - x1
+                r, c = y1, x1
+                while 0 <= r < row and 0 <= c < col:
+                    antinodes.add((r, c))
+                    r += dy
+                    c += dx
 
     print(len(antinodes))
 
